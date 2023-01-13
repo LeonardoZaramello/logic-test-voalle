b8ce4d82a0b8c50a22e4538cb42953b1d0d400cb
Leonardo Zaramello <leeurw@hotmail.com>
2023-01-13 16:09:48 -0300

    Add: user interact loop

--- DiamondPrinter/DiamondWriter.cs
+++ DiamondPrinter/DiamondWriter.cs
@@ -1,9 +1,28 @@
-namespace DiamondCommands;
+namespace DiamondPrinter;
 
 public class DiamondWriter
 {
+    public char[] alphabet = new char[26];
+    public char choosedLetter;
+
+    public void GetAlphabetUpperCase()
+    {
+        var index = 0;
+
+        for (int i = 65; i <= 90; i++)
+        {
+            alphabet[index] = (char)i;
+            index++;
+        }
+    }
+
     public void Greeting()
     {
         Console.WriteLine("Welcome to Diamond Writer!!");
+    }
+
+    public void ChooseLetter()
+    {
+        Console.WriteLine("Please choose a *single letter* to be the edge of the Diamond!");
     }
 }
--- DiamondPrinter/Program.cs
+++ DiamondPrinter/Program.cs
@@ -1,13 +1,40 @@
-﻿namespace DiamondWriter;
-using DiamondCommands;
-
-class Program
+﻿namespace DiamondPrinter
 {
-    static void Main(string[] args)
-    {
-        var diamond = new DiamondWriter();
 
-        diamond.Greeting();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var diamond = new DiamondWriter();
+
+            diamond.GetAlphabetUpperCase();
+            diamond.Greeting();
+            diamond.ChooseLetter();
+
+            do
+            {
+                string input = Console.ReadLine() ?? "";
+
+                try
+                {
+                    var letter = char.Parse(input.ToUpper());
+
+                    
+                    if(input.Length == 1 && diamond.alphabet.Contains(letter))
+                    {
+                        diamond.choosedLetter = letter;
+                        break;
+                    }
+                
+                    Console.WriteLine("Only single letters are allowed");
+                }
+                catch (System.Exception)
+                {
+                    Console.WriteLine("Only single letters are allowed");
+                }
+            } while (true);
+
+            Console.WriteLine(diamond.choosedLetter);
+        }
     }
 }
-
