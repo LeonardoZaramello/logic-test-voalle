9963d5e260f7ffe558f394c2f89a60157af963a1
Leonardo Zaramello <leeurw@hotmail.com>
2023-01-13 14:37:40 -0300

    Init project Diamont Writer

---
+++ DiamondPrinter/DiamondWriter.cs
@@ -0,0 +1,9 @@
+namespace DiamondCommands;
+
+public class DiamondWriter
+{
+    public void Greeting()
+    {
+        Console.WriteLine("Welcome to Diamond Writer!!");
+    }
+}
---
+++ DiamondPrinter/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace DiamondWriter;
+using DiamondCommands;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var diamond = new DiamondWriter();
+
+        diamond.Greeting();
+    }
+}
+
